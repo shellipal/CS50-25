a6355c09b33dc9c81263e0e074b978dc2437c039
shellipal <palshelli5@gmail.com>
2025-03-26 23:58:24 +0530

    just check

--- 01_C/01_hello.c
+++ 01_C/01_hello.c
@@ -1,6 +1,9 @@
+#include <string.h>
 #include <stdio.h>
 
 int main(void)
 {
-    printf("Hello World !\n");
+    // printf("Hello World !\n");
+    int answer = printf("what's your name?");
+    printf(" Hello , %d\n", answer);
 }
